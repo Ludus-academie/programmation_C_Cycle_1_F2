1d315272d78f24d2141d395c727cad18c7079e01
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-11-13 16:11:22 +0100

    Fcts et procédures
    Introduction fcts et procédures
    Tableau a une dimension et fonction
    Tableau passsage par adresse

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,123 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define TAILLE 10
+
+//prototypes
+int somme_int(int,int);
+float somme_float(float flt_va, float flt_vb);
+void somme_procedure(int n_va, int n_vb);
+void init_tabEntier(int n_tabEntier[TAILLE]);
+int fctInit_tabEntier(int n_tabEntier[TAILLE]);
 
 int main()
 {
-    printf("Hello world!\n");
+    int n_a=0,n_b=0;
+    int n_Res=0;
+    float flt_Res=0;
+    int n_tabEntier[TAILLE];
+
+    n_a=5;
+    n_b=11;
+
+    n_Res=somme_int(n_a,n_b);
+
+    printf("%d\n",n_Res);
+
+    flt_Res=somme_float(5.0,2.0);
+
+    printf("%f\n",flt_Res);
+
+    somme_procedure(n_a,n_b);
+
+
+    init_tabEntier(n_tabEntier);
+
+    int result=fctInit_tabEntier(n_tabEntier);
+
+
+
     return 0;
+}
+
+
+
+//fonction somme entier
+
+int somme_int(int n_va, int n_vb){
+
+   /* int n_Som=0;
+
+    n_Som=n_va+n_vb;
+
+    return n_Som;*/
+
+    return n_va+n_vb;
+
+}
+
+float somme_float(float flt_va, float flt_vb){
+
+   /* float n_Som=0;
+
+    n_Som=flt_va+flt_vb;
+
+    return n_Som;*/
+
+    return flt_va+flt_vb;
+
+}
+
+int fctInit_tabEntier(int n_tabEntier[TAILLE]){
+
+    int n_i=0;
+    int n_Result=0;
+
+    for(n_i=0;n_i<TAILLE;n_i++){
+
+        n_tabEntier[n_i]=0;
+
+        if(n_tabEntier[n_i]==0){
+                n_Result=1;
+        }
+        else{
+            n_Result=0;
+        }
+    }
+
+    return n_Result;
+
+
+}
+
+
+
+
+
+//Procedure
+
+void somme_procedure(int n_va, int n_vb){
+
+    /*int n_Som=0;
+
+    n_Som=n_va+n_vb;
+
+    printf("Res somme : %d",n_Som);*/
+
+    printf("Res somme : %d",n_va+n_vb);
+
+
+}
+
+
+
+void init_tabEntier(int n_tabEntier[TAILLE]){
+
+    int n_i=0;
+
+    for(n_i=0;n_i<TAILLE;n_i++){
+
+        n_tabEntier[n_i]=0;
+    }
+
+
 }

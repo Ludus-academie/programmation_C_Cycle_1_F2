38d3277d5132bc568676a8807a026779ed8c80cd
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-11-18 13:00:32 +0100

    Classe d'allocation et passage par adresse
    Classe d'allocation
    static
    extern
    volatile
    auto
    Introduction pointeurs (passage par adresse)

---
+++ programmation_c/define.h
@@ -0,0 +1,11 @@
+#ifndef DEFINE_H_INCLUDED
+#define DEFINE_H_INCLUDED
+#define TAILLE 10
+
+extern int somme_int(int,int);
+extern float somme_float(float flt_va, float flt_vb);
+extern void somme_procedure(int n_va, int n_vb);
+extern void init_tabEntier(int n_tabEntier[TAILLE]);
+extern int fctInit_tabEntier(int n_tabEntier[TAILLE]);
+
+#endif // DEFINE_H_INCLUDED

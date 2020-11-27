e83f1f926bdd5d1e90abd12e8641f79228f2b149
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-11-27 06:42:16 +0100

    SDL2 surface&Texture
    SDL2
    Surface
    Texture

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -6,42 +6,133 @@
 #define WINDOW_HEIGHT 780
 
 
-SDL_Window *pWindow=NULL;
-SDL_Renderer *pRenderer=NULL;
-//SDL_Surface *pSurface=NULL;
+typedef struct renderer{
+
+    SDL_Window *pWindow;
+    SDL_Renderer *pRenderer;
+
+}renderer;
+
+typedef struct rendererObject{
+
+    SDL_Surface *pSurface;
+    SDL_Texture *pTexture;
+
+}rendererObject;
+
 
 
 
 int main(int argc, char *argv[])
 {
+    renderer tRender;
+    rendererObject tRobject;
+
     if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
 
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
     }else{
 
-        pWindow=SDL_CreateWindow("An SDL2 window",640,480,WINDOW_WIDTH,WINDOW_HEIGHT,
+        tRender.pWindow=SDL_CreateWindow("An SDL2 window",640,480,WINDOW_WIDTH,WINDOW_HEIGHT,
                                  SDL_WINDOW_SHOWN);
 
-        if(pWindow){
+        if(tRender.pWindow){
 
-            pRenderer=SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_PRESENTVSYNC);
+            tRender.pRenderer=SDL_CreateRenderer(tRender.pWindow,-1,SDL_RENDERER_PRESENTVSYNC);
         }
 
 
 
     }
 
-    if(pRenderer){
+    if(tRender.pRenderer){
+
+         //Set Color
+        SDL_SetRenderDrawColor(tRender.pRenderer,205,92,92,SDL_ALPHA_OPAQUE);
+
+          //Clear Render
+        SDL_RenderClear(tRender.pRenderer);
+
+        /*tRobject.pSurface=SDL_LoadBMP("./assets/rider.bmp");
+
+        if(!tRobject.pSurface){
+            SDL_Log("Unable to set surface: %s", SDL_GetError());
+            return 1;
+        }else{
+
+            tRobject.pTexture=SDL_CreateTextureFromSurface(tRender.pRenderer,tRobject.pSurface);
+            SDL_FreeSurface(tRobject.pSurface);
+
+            if(!tRobject.pTexture){
+                SDL_Log("Unable to set texture: %s", SDL_GetError());
+                return 1;
+            }else{
+
+                SDL_Rect rectSource;
+                SDL_Rect rectDest;
+
+                rectSource.x=0;
+                rectSource.y=0;
+                rectSource.w=123;
+                rectSource.h=87;
+
+                rectDest.x=0;
+                rectDest.y=0;
+                rectDest.w=rectSource.w;
+                rectDest.h=rectSource.h;
+
+                SDL_RenderCopy(tRender.pRenderer,tRobject.pTexture,&rectSource,&rectDest);
+
+            }
 
 
-        //Set Color
-        SDL_SetRenderDrawColor(pRenderer,205,92,92,SDL_ALPHA_OPAQUE);
-        //Clear Render
-        SDL_RenderClear(pRenderer);
+        }*/
+
+
+        tRobject.pSurface=SDL_CreateRGBSurface (0, 100, 100, 32, 0, 0, 0, 0);
+
+        if(!tRobject.pSurface){
+            SDL_Log("Unable to set surface: %s", SDL_GetError());
+            return 1;
+        }else{
+            SDL_Rect surface_rect = {0, 0, 100, 100};
+            SDL_FillRect(tRobject.pSurface, &surface_rect, SDL_MapRGB(tRobject.pSurface->format, 255, 255, 0));
+
+            tRobject.pTexture=SDL_CreateTextureFromSurface(tRender.pRenderer,tRobject.pSurface);
+            SDL_FreeSurface(tRobject.pSurface);
+
+            if(!tRobject.pTexture){
+                SDL_Log("Unable to set texture: %s", SDL_GetError());
+                return 1;
+            }else{
+
+                SDL_Rect rectSource;
+                SDL_Rect rectDest;
+
+                rectSource.x=0;
+                rectSource.y=0;
+                rectSource.w=200;
+                rectSource.h=200;
+
+                rectDest.x=15;
+                rectDest.y=15;
+                rectDest.w=rectSource.w;
+                rectDest.h=rectSource.h;
+
+                SDL_RenderCopy(tRender.pRenderer,tRobject.pTexture,&rectSource,&rectDest);
+
+            }
+
+        }
+
+
+
 
         //Update render
-        SDL_RenderPresent(pRenderer);
+        SDL_RenderPresent(tRender.pRenderer);
+
+
 
     }
 
@@ -49,12 +140,12 @@ int main(int argc, char *argv[])
 
     SDL_Delay(10000);
 
-    if(pRenderer){
-        SDL_DestroyRenderer(pRenderer);
+    if(tRender.pRenderer){
+        SDL_DestroyRenderer(tRender.pRenderer);
     }
 
-    if(pWindow){
-      SDL_DestroyWindow(pWindow);
+    if(tRender.pWindow){
+      SDL_DestroyWindow(tRender.pWindow);
     }
 
 

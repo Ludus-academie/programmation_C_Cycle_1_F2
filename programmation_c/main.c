a185bcdf849eebc5564d54091ad8b38f8debfe9e
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-12-03 11:30:15 +0100

    pointeurs_generiques et fonctions
    Exemple pointeurs pointeurs génériques
    Pointeurs génériques et fonctions

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,58 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Les pointeurs sur fonctions
+
+//pointeur générique
+void raz(void *adr, int n_size);
 
 //prototypes
-void affiche(int n_val);
+void affiche(int *n_val);
+void affiche2(double *n_val);
 
 //un pointeur de la fonction affiche
-void (*pAffiche)(int);
+//void (*pAffiche)(int);
+void (*pAffiche)(void*);
 
 
-//exemple fonction
-int somme(int n_a, int n_b);
 
-int (*pf)(int,int);
 
-int fctEntier(int (*pnf)(int),int n_val);
-
-int fctE(int n_a);
 
 int main()
 {
-   //affiche(12);
-   pAffiche=&affiche;
 
-   pf=&somme;
+    //pointeur générique
+   double dbl_val;
+   int t[10];
 
-   printf("somme:%d ", pf(2,3));
+    raz(t,10*sizeof(int));
+    raz(&dbl_val,sizeof(double));
 
-   int res=fctEntier(&fctE,2);
+    //pointeurs génériques et fonctions
+   int n_val=12;
+
+   pAffiche=(void*)&affiche;
+
+   pAffiche(&n_val);
+
+   double dbl_reel=8.5;
+
+   pAffiche=(void*)&affiche2;
+
+   pAffiche(&dbl_reel);
+
+
 
    return 0;
 }
 
+void raz(void *adr, int n_size){
 
-void affiche(int n_val){
-
-    printf("%d\n",n_val);
-
-}
-
-int somme(int n_a, int n_b){
-
-    return n_a + n_b;
-}
-
-int fctEntier(int (*pnf)(int),int n_val){
-
-    return (*pnf)(n_val);
+    int i;
+    char *ad=adr;
+    for(i=0;i<n_size;i++)
+        *(ad+i)=0; //ou ad[i]
 
 }
 
+void affiche(int *n_val){
 
-int fctE(int n_a){
+    char *adr=(char*)n_val;
+    printf("%d\n",*n_val);
 
-    return n_a;
 }
+
+void affiche2(double *n_val){
+
+    char *adr=(char*)n_val;
+    printf("%lf\n",*n_val);
+
+}
+
+

f2e09978244ea3e0fdd441fa8634b17aaf2c74f0
Lehmann Nicolas <nico.nglconsulting@gmail.com>
2020-12-03 09:58:35 +0100

    Pointeurs sur fonctions
    Pointeurs sur fonctions et pointeurs sur fonctions en arguments

--- programmation_c/main.c
+++ programmation_c/main.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Les pointeurs sur fonctions
+
+//prototypes
+void affiche(int n_val);
+
+//un pointeur de la fonction affiche
+void (*pAffiche)(int);
+
+
+//exemple fonction
+int somme(int n_a, int n_b);
+
+int (*pf)(int,int);
+
+int fctEntier(int (*pnf)(int),int n_val);
+
+int fctE(int n_a);
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+   //affiche(12);
+   pAffiche=&affiche;
+
+   pf=&somme;
+
+   printf("somme:%d ", pf(2,3));
+
+   int res=fctEntier(&fctE,2);
+
+   return 0;
+}
+
+
+void affiche(int n_val){
+
+    printf("%d\n",n_val);
+
+}
+
+int somme(int n_a, int n_b){
+
+    return n_a + n_b;
+}
+
+int fctEntier(int (*pnf)(int),int n_val){
+
+    return (*pnf)(n_val);
+
+}
+
+
+int fctE(int n_a){
+
+    return n_a;
 }
